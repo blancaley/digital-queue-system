f073558b44252aadeb9b67bca43edef20b5a6aaf
Blanca Ley <blanca.ley@outlook.com>
2021-10-13 16:45:59 +0200

    Add new person and queue number

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 let queue = [];
+const personName = document.querySelector("#name");
 
+function add() {
+    let newPerson = {
+    queueNum: queue.length + 1,
+    person: personName.value
+    }
+    queue.push(newPerson);
+}
