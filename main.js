5081f8a1cd0c0d2872818a5f1f9bf8330fde9719
Blanca Ley <blanca.ley@outlook.com>
2021-10-13 17:04:54 +0200

    Show new person name

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 let queue = [];
 const personName = document.querySelector("#name");
+let list = document.querySelector("#list");
+
+function showQueue() {
+    queue.forEach(element => {
+        let personInList = document.createElement("li");
+        personInList.innerText = element.personName;
+        list.appendChild(personInList);
+    });
+}
 
 function add() {
     let newPerson = {
-    queueNum: queue.length + 1,
-    person: personName.value
+        queueNum: queue.length + 1,
+        personName: personName.value
     }
     queue.push(newPerson);
+    showQueue();
 }
